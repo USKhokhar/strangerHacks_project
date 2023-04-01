a9f0b2683e0e625d775f9873c251fde3ebbb8b6b
Vishruth S <vs.vishruth.s@gmail.com>
2023-04-01 17:27:39 +0530

    extract question, local storage fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Chat from './Components/Chat/Chat';
 import './App.css'
 
 function App() {
+  const [active, setActive] = useState(false)
 
+  const check = () => {
+    const targetSiteUrl = "https://leetcode.com/problems/"
+    /* eslint-disable no-undef */
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function (tabs) {
+      var tabURL = tabs[0].url;
+      if (tabURL.startsWith(targetSiteUrl)) {
+        setActive(true)
+      } else {
+        setActive(false)
+      }
+    });
+  }
+
+  useEffect(() => {
+    check()
+  }, [])
 
   return (
     <div className="App">
-      <Chat />
+      {active ? <Chat /> : <div className="not-active">Please go to leetcode.com to use this extension</div>}
     </div>
   );
 }
--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,21 +1,61 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useState } from 'react';
 import { BsSend } from "react-icons/bs";
+import { extractCodeFromHtml } from '../../utils/extractCodeFromHtml';
+import { extractTextFromHtml } from '../../utils/extractTextFromHtml';
 import useAutosizeTextArea from '../../utils/useAutoTextArea';
 import './Chat.css'
 import ChatMessage from './ChatMessage';
+/* eslint-disable no-undef */
 
-const API_KEY = "";
+const API_KEY = ;
 
 const Chat = () => {
-
+    const [tabId, setTabId] = useState(0)
     const [messages, setMessages] = useState([]);
     const [inputMsg, setInputMsg] = useState("");
     const [loadingMsg, setLoadingMsg] = useState(false)
+    const [triggerSend, setTriggerSend] = useState(false)
+
+    const [questionTitle, setQuestionTitle] = useState('')
+    const [questionData, setQuestionData] = useState('')
+    const [code, setCode] = useState('')
 
     const textAreaRef = useRef(null);
 
     useAutosizeTextArea(textAreaRef.current, inputMsg);
+
+    useEffect(() => {
+        handleSend(0)
+        // eslint-disable-next-line
+    }, [triggerSend])
+
+    useEffect(() => {
+        async function fetchData() {
+            getQuestionData()
+            getQuestionTitle()
+            console.log("FISH - ", tabId)
+            await chrome.storage.local.get(`${tabId}messages`, result => {
+                console.log(`${tabId}messages`, result)
+                if (result[`${tabId}messages`]) {
+                    setMessages(JSON.parse(result[`${tabId}messages`]))
+                }
+            })
+        }
+        fetchData()
+        // eslint-disable-next-line
+    }, [tabId])
+
+    useEffect(() => {
+        async function fetchTabData() {
+            await chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+                let activeTabId = tabs[0].id;
+                setTabId(activeTabId)
+            })
+        }
+        fetchTabData()
+        // eslint-disable-next-line
+    }, [])
 
     async function processMessageChatgpt(chatMessages) {
         let apiMessages = chatMessages.map((messageObj) => {
@@ -30,7 +70,7 @@ const Chat = () => {
 
         const systemMessage = {
             role: "system",
-            content: "I'm a student using leetcode to prepare for software engineering interviews. Only answer questions that are relevant to the interview preparation"
+            content: "I'm a student using leetcode to prepare for software engineering interviews. Only answer questions that are relevant to the interview preparation. I'm currently trying the " + questionTitle + " question. Here is the full description of the question: " + questionData
         }
 
         const apiRequestBody = {
@@ -48,19 +88,23 @@ const Chat = () => {
             body: JSON.stringify(apiRequestBody)
         }).then((data) => {
             return data.json()
-        }).then((data) => {
-            console.log("ehiowgopiwe")
-            console.log(data)
-            setMessages([...chatMessages, {
+        }).then(async (data) => {
+            let msgs = [...chatMessages, {
                 sender: "ChatGPT",
                 message: data.choices[0].message.content
-            }])
+            }]
+            setMessages(msgs)
+            console.log("CISH ", tabId)
+            await chrome.storage.local.set({
+                [`${tabId}messages`]: JSON.stringify(msgs)
+            })
         })
     }
 
     const handleSend = async (e) => {
-        console.log("Hello")
-        e.preventDefault();
+        if (inputMsg === '')
+            return;
+        if (e !== 0) e.preventDefault();
         setLoadingMsg(true)
         const newMessage = {
             message: inputMsg,
@@ -83,6 +127,95 @@ const Chat = () => {
         }
     }
 
+    // mr-2 text-lg font-medium text-label-1 dark:text-dark-label-1
+    const getQuestionTitle = async (e) => {
+        await chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+            let activeTabId = tabs[0].id;
+            await chrome.scripting.executeScript(
+                {
+                    target: { tabId: activeTabId },
+                    func: () => {
+                        const element = document.getElementsByClassName('mr-2 text-lg font-medium text-label-1 dark:text-dark-label-1')[0];
+                        if (element) {
+                            return element.innerText;
+                        }
+                        return null;
+                    }
+                },
+                (result) => {
+                    let qt = result[0].result.slice(result[0].result.indexOf('.') + 1)
+                    setQuestionTitle(qt)
+
+                }
+            );
+        });
+    };
+
+    const getQuestionData = async (e) => {
+        await chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+            let activeTabId = tabs[0].id;
+            await chrome.scripting.executeScript(
+                {
+                    target: { tabId: activeTabId },
+                    func: () => {
+                        const element = document.getElementsByClassName('_1l1MA')[0];
+                        if (element) {
+                            return element.innerHTML;
+                        }
+                        return null;
+                    }
+                },
+                (result) => {
+                    const rawString = extractTextFromHtml(result[0].result)
+                    const questionText = rawString.slice(0, rawString.indexOf('Example 1'))
+                    setQuestionData(questionText)
+                    // setInputMsg("Give me a hint for the " + result[0].result.slice(result[0].result.indexOf('.') + 1) + " problem on Leetcode");
+                    // setTriggerSend(!triggerSend)
+                }
+            );
+        });
+    }
+
+    const getCode = async (e) => {
+        await chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+            let activeTabId = tabs[0].id;
+            await chrome.scripting.executeScript(
+                {
+                    target: { tabId: activeTabId },
+                    func: () => {
+                        const element = document.getElementsByClassName('view-lines monaco-mouse-cursor-text')[0];
+                        if (element) {
+                            return element.innerHTML;
+                        }
+                        return null;
+                    }
+                },
+                (result) => {
+                    //========= WIP ========= //
+                    // const code = extractCodeFromHtml(result[0].result)
+                    // setCode(code)
+                    // setInputMsg(code + ". What's the error?")
+                    // sessionStorage.set('questionData', questionText)
+                    // setInputMsg("Give me a hint for the " + result[0].result.slice(result[0].result.indexOf('.') + 1) + " problem on Leetcode");
+                    // setTriggerSend(!triggerSend)
+                }
+            );
+        });
+    }
+
+    const prefilledPrompt = (promptMessage) => {
+        if (questionTitle === '') {
+            getQuestionData()
+            getQuestionTitle()
+        }
+        setInputMsg(promptMessage)
+        setTriggerSend(!triggerSend)
+    }
+
+    const debugSolution = () => {
+        getCode()
+    }
+
     return (
         <div className="chat-container">
             <h2>Mermaid - Your leetcode buddy</h2>
@@ -90,12 +223,11 @@ const Chat = () => {
                 <ChatMessage messages={messages} />
             </div>
             {loadingMsg && <div>CHATGPT IS THINKING ...</div>}
-            {/* A menu with 3 buttons */}
             <div className="chat-menu">
-                <button className="chat-menu-btn">📖 Get started hint</button>
-                <button className="chat-menu-btn">📚 Optimize hint</button>
-                <button className="chat-menu-btn">📝 Solution</button>
-                <button className="chat-menu-btn">📝 Debug</button>
+                <button className="chat-menu-btn" onClick={() => prefilledPrompt("Give me a hint on how to start solving this question")}>📖 How do I start</button>
+                <button className="chat-menu-btn" onClick={() => prefilledPrompt("What's the ideal optimized solution for this question?")}>📝 Solution</button>
+                <button className="chat-menu-btn" onClick={() => prefilledPrompt("What's the ideal time complexity expected?")}>📝 Ideal TC</button>
+                <button className="chat-menu-btn" onClick={() => prefilledPrompt("Ask me something an interviewer would ask based on this question")}>📝 Interview Me</button>
             </div>
             <div className='chat-input-box'>
                 <form>
@@ -107,6 +239,7 @@ const Chat = () => {
                             onKeyDown={enterPressed}
                             rows={1}
                             ref={textAreaRef}
+                            required
                         ></textarea>
                         <button className="chat-send-btn" onClick={handleSend}><BsSend /></button>
                     </div>
